f474009742ed8578a09bae9861102b5baada9110
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-04-11 09:51:39 +0200

    Update scraper.py import By

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
