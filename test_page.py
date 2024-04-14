1019308c6c7525cb8613f62f9676e23db2f7b0a1
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-04-14 02:50:43 +0200

    Update test_page.py chrome driver

--- test_page.py
+++ test_page.py
@@ -3,30 +3,43 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from sb_page import SupabetsPageLocators
+import pandas
+import csv
 
-# pytest + chrome logic
-@pytest.fixture
-def browser():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    yield driver
-    driver.quit()
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 #testlogic
 
-def test_scrape_odd_info(browser):
-    browser.get(SupabetsPageLocators.URL)
+def test_scrape_odd_info(driver):
+    driver.get(SupabetsPageLocators.URL)
 
-    sport_name_element = browser.find_element_by_css_selector(SupabetsPageLocators.SPORT_NAME_SELECTOR)
-    league_name_element = browser.find_element_by_css_selector(SupabetsPageLocators.LEAGUE_NAME_SELECTOR)
-    home_team_name_element = browser.find_element_by_css_selector(SupabetsPageLocators.HOME_TEAM_NAME_SELECTOR)
-    away_team_name_element = browser.find_element_by_css_selector(SupabetsPageLocators.AWAY_TEAM_NAME_SELECTOR)
-    market_type_element = browser.find_element_by_css_selector(SupabetsPageLocators.MARKET_TYPE_SELECTOR)
-    odd_type_element = browser.find_element_by_css_selector(SupabetsPageLocators.ODD_TYPE_SELECTOR)
-    odd_value_element = browser.find_element_by_css_selector(SupabetsPageLocators.ODD_VALUE_SELECTOR)
+    sport_name_element = driver.find_element(By.CSS_SELECTOR, SupabetsPageLocators.SPORT_NAME_SELECTOR)
+    league_name_element = driver.find_element(By.CSS_SELECTOR, SupabetsPageLocators.LEAGUE_NAME_SELECTOR)
+    home_team_name_element = driver.find_element(By.CSS_SELECTOR, SupabetsPageLocators.HOME_TEAM_NAME_SELECTOR)
+    away_team_name_element = driver.find_element(By.CSS_SELECTOR, SupabetsPageLocators.AWAY_TEAM_NAME_SELECTOR)
+    market_type_element = driver.find_element(By.CSS_SELECTOR, SupabetsPageLocators.MARKET_TYPE_SELECTOR)
+    odd_type_element = driver.find_element(By.CSS_SELECTOR, SupabetsPageLocators.ODD_TYPE_SELECTOR)
+    odd_value_element = driver.find_element(By.CSS_SELECTOR, SupabetsPageLocators.ODD_VALUE_SELECTOR)
 
     sport_name = sport_name_element.text
     league_name = league_name_element.text
