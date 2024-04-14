910c606523d4f415b6554ca720be3dec41cc78b2
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-04-14 02:18:02 +0200

    Update sb_page.py

--- sb_page.py
+++ sb_page.py
@@ -4,7 +4,6 @@ class SupabetsPageLocators:
     LEAGUE_NAME_SELECTOR = "span.eventName.ng-binding"
     HOME_TEAM_NAME_SELECTOR = "div.plr_1.ng-binding"
     AWAY_TEAM_NAME_SELECTOR = "div.plr_2.ng-binding"
-    MARKET_TYPE_SELECTOR = 
-"div.titleCq.ng-binding"
+    MARKET_TYPE_SELECTOR = "div.titleCq.ng-binding"
     ODDS_TYPE_SELECTOR = "div.oddType.ng-binding"
     ODDS_VALUE_SELECTOR = "div.oddValue.ng-binding"
